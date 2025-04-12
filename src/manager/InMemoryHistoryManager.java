2b9852ff9cb9c85712c144e75028d95483ce2e59
Irina Boyko <t.i.sergeevna@yandex.ru>
2025-04-12 15:47:49 +0300

    Перемещение класса Node в класс менеджера истории

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -91,35 +91,36 @@ public class InMemoryHistoryManager implements HistoryManager {
             }
         }
     }
+    static class Node {
+        private Task task;
+        private Node prev;
+        private Node next;
+
+        public Task getTask() {
+            return task;
+        }
+
+        public void setTask(Task task) {
+            this.task = task;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
 
-class Node {
-    private Task task;
-    private Node prev;
-    private Node next;
 
-    public Task getTask() {
-        return task;
-    }
-
-    public void setTask(Task task) {
-        this.task = task;
-    }
-
-    public Node getPrev() {
-        return prev;
-    }
-
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-}
 
